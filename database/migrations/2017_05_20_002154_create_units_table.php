39b3f47e65c4b2b31fd761cd698900bd0c527001
josezg <josezgalvezz@gmail.com>
2017-06-04 23:42:54 -0500

    Some Updates
    Migration table name change from "Laboratories" to "Units".
    Bower initial configuration (bower.json and .bowerrc).
    Laravel Auth Maked via artisan.

--- database/migrations/2017_05_20_002154_create_units_table.php
+++ database/migrations/2017_05_20_002154_create_units_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLaboratoriesTable extends Migration
+class CreateUnitsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateLaboratoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('laboratories', function (Blueprint $table) {
+        Schema::create('units', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateLaboratoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('laboratories');
+        Schema::dropIfExists('units');
     }
 }
